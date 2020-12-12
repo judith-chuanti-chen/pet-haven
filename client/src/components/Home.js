3060b8563d07c95b414440fb68f8ae095821b612
Judith Chuan Ti Chen <chen.chuan@husky.neu.edu>
2020-12-11 19:44:38 -0800

    Reformatted search

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,7 +14,6 @@ const Home = () => {
                 </Jumbotron>
             <Container>
                 <SearchBar />
-                <PetThumbnail />
             </Container>
         </>
     );
