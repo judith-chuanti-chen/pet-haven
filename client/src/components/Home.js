ce7ed7bb1d870c2459e7db3c8637f68e0b1efca6
Judith Chuan Ti Chen <chen.chuan@husky.neu.edu>
2020-12-14 15:56:44 -0800

    fixed bug of favPets

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,30 @@
 import React, { useEffect } from 'react';
 import PetThumbnail from './PetThumbnail';
 import SearchBar from './SearchBar';
+import { getAllFavPets } from '../utils/manageFavoritePet';
+import { useDispatch } from 'react-redux';
+import { addFavPet } from '../store/actions/favPet_actions';
 import {Container, Jumbotron, Row, Col} from 'react-bootstrap';
 
-const Home = (props) => {
+const Home = ({user, favPets}) => {
+    const dispatch = useDispatch();
+    const populateInitialFavPets = () => {
+    // Populate initial favPets in the app if user is logged in
+        if(!user.auth) return;
+        if(favPets.size === 0){
+            getAllFavPets(user.userData._id, (petIds) => {
+                petIds.forEach(petId => {
+                    dispatch(addFavPet(petId));
+                });   
+            });     
+        }
+        console.log("Populated initial favPets: ");
+        console.log(favPets);
+    }
+
+    useEffect(() => {
+        populateInitialFavPets();
+    }, [user.auth]);
     return(
         <>
             <Jumbotron fluid>
